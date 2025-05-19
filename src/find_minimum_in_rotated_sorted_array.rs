9fe8ada294d5a12a787a825d9cd0a0fdd7c641ea
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2025-05-18 21:55:41 -0300

    find minimum in rotated sorted array binary search

--- src/find_minimum_in_rotated_sorted_array.rs
+++ src/find_minimum_in_rotated_sorted_array.rs
@@ -1,12 +1,12 @@
-use std::cmp::Ordering;
+use std::cmp;
 
 pub struct Solution;
 
 impl Solution {
     // Approach 1 - Brute Force
     pub fn find_min_brute_force(mut nums: Vec<i32>) -> i32 {
-        let mut right = 0;
-        let mut changed = false;
+        let (mut right, mut changed) = (0, false);
+
         for n in 0..(nums.len() - 1) {
             if nums[n] < nums[n + 1] && !changed {
                 continue;
@@ -25,26 +25,23 @@ impl Solution {
 
     // Approach 2 - Binary Search
     pub fn find_min(mut nums: Vec<i32>) -> i32 {
-        let mut low = 0;
-        let mut high = nums.len() - 1;
-
-        while low <= high && high - low > 1 {
+        let (mut low, mut high) = (0, nums.len() - 1);
+        let mut min = nums[low];
+        
+        while low <= high && high - low >= 1 {
             let middle = low + (high - low) / 2;
 
-            match nums[middle as usize].cmp(&nums[high]) {
-                Ordering::Less => high = middle,
-                Ordering::Equal => (),
-                Ordering::Greater => low = middle,
+            min = cmp::min(min, nums[middle]);
+            
+            if nums[middle] >= nums[low] {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
             }
+            
+            min = cmp::min(min, nums[low]);
         }
 
-        let rotation_value = nums.len() - high;
-        nums.rotate_right(rotation_value);
-
-        println!("low: {}", low);
-        println!("high: {}", high);
-        println!("nums: {:?}", nums);
-
-        nums[0]
+        min
     }
 }
